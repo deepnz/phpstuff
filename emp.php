6a9c0252da9b93a28e72f9b1ea73fd6069834912
“Deepak” <109643803+deepnz@users.noreply.github.com>
2023-07-16 00:01:00 -0400

    first

---
+++ emp.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>EMPLOYEE DETAIL ENTRY FORM</title>
+</head>
+<body>
+<form action="det.php" method="post">
+<labelfor="ename">Employee Name:</label>
+<input type="text" name="ename"></br>
+<label for="eid">Employee Id:</label>
+<input type="text" name="eid"></br>
+<label for="eloc">Employee Location:</label>
+<input type="text" name="eloc"></br>
+<label for="edept">Employee Department:</label>
+<input type="text" name="edept"></br>
+<input type="submit" name="submit" value="send">
+</form>
+</body>
+<html>
